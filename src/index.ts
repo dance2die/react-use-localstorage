808dfbb7f7d408feb9b822d00275270e31929730
Aaron Roberts <aaronmroberts1@gmail.com>
2019-02-10 11:39:22 -0500

    get value from local storage or use initial value

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function useLocalStorage(key: string, initialValue: string = "") {
   const [item, setValue] = React.useState(() => {
-    localStorage.getItem(key) || localStorage.setItem(key, initialValue); 
-    return initialValue;
+    const value = localStorage.getItem(key) || initialValue;
+    localStorage.setItem(key, value);
+    return value;
   });
 
   const setItem = (item: string) => {
