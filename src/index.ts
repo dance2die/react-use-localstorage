aa9f4688c7e90cba43a1f9ab431b97558979f199
TheAifam5 <theaifam5@gmail.com>
2019-02-10 19:42:51 +0100

    Added TSLint. Added EditorConfig. Added pre-commit hooks. Fixed shadowed name "item".

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-export default function useLocalStorage(key: string, initialValue: string = "") : [string, (item: string) => void] {
+export default function useLocalStorage(key: string, initialValue: string = '') {
   const [item, setValue] = React.useState(() => {
     const value = localStorage.getItem(key) || initialValue;
     localStorage.setItem(key, value);
     return value;
   });
 
-  const setItem = (item: string) => {
-    setValue(item);
-    window.localStorage.setItem(key, item);
+  const setItem = (newValue: string) => {
+    setValue(newValue);
+    window.localStorage.setItem(key, newValue);
   };
 
   return [item, setItem];
