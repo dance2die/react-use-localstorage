fddb48320a0bfbff640202c48f51d2be9389db02
Sung Kim <sungmkim80@gmail.com>
2019-02-10 10:27:26 -0500

    🛠 Fixed an issue where there is no default value causing form field causing uncontrolled component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function useLocalStorage(key: string) {
-  const [item, setValue] = React.useState(() => window.localStorage.getItem(key));
+export default function useLocalStorage(key: string, initialValue: string = "") {
+  const [item, setValue] = React.useState(() => window.localStorage.getItem(key) || initialValue);
 
   const setItem = (item: string) => {
     setValue(item);
