2a960e986f026932015153ea8b44b7e7ffa49fae
dance2die <sungmkim80@gmail.com>
2019-03-24 20:54:47 -0400

    The test 'setItem should overwrite existing value with new value' passes

--- packages/react-use-storage/__tests__/react-use-storage.test.ts
+++ packages/react-use-storage/__tests__/react-use-storage.test.ts
@@ -3,25 +3,42 @@
 import { renderHook, cleanup, act } from "react-hooks-testing-library";
 import useStorage from "../src/index";
 
+afterEach(cleanup);
+
 describe("react-use-storage", () => {
   beforeEach(() => {
     localStorage.clear();
     sessionStorage.clear();
   });
-  afterEach(cleanup);
 
-  test("should save to localStorage", () => {
-    const [KEY, VALUE] = ["foo", "bar"];
-    const { result } = renderHook(() => useStorage(KEY, VALUE));
-    expect(localStorage.setItem).toHaveBeenLastCalledWith(
-      KEY,
-      JSON.stringify("bar")
-    );
-    expect(localStorage.__STORE__[KEY]).toBe(JSON.stringify("bar"));
-    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+  describe(`localStorage tests`, () => {
+    test(`Initially, "useStorage" should save to localStorage`, () => {
+      const [KEY, VALUE] = ["foo", "bar"];
+      const { result } = renderHook(() => useStorage(KEY, VALUE));
 
-    // act(() => result.current[0].setItem(KEY, VALUE));
+      expect(localStorage.setItem).toHaveBeenLastCalledWith(
+        KEY,
+        JSON.stringify(VALUE)
+      );
+      expect(localStorage.__STORE__[KEY]).toBe(JSON.stringify(VALUE));
+      expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    });
+
+    test(`setItem should overwrite existing value with new value`, () => {
+      const key = "test key";
+      const oldValue = { a: 1 };
+      const newValue = { x: "x", y: "y" };
+
+      const { result } = renderHook(() => useStorage(key, oldValue));
+      expect(localStorage.setItem).toHaveBeenLastCalledWith(
+        key,
+        JSON.stringify(oldValue)
+      );
+
+      console.log(`result ==>`, result);
+
+      act(() => result.current[1](newValue));
+      expect(localStorage.__STORE__[key]).toBe(JSON.stringify(newValue));
+    });
   });
-
-  // it("Happy Path", () => {});
 });
--- packages/react-use-storage/src/index.ts
+++ packages/react-use-storage/src/index.ts
@@ -2,23 +2,23 @@ import React, { Dispatch } from "react";
 
 export default function useStorage(
   key: string,
-  initialValue: any,
+  initialValue: any = undefined,
   // Storage strategy
   storage: Storage = localStorage
 ): [any, Dispatch<any>] {
   const [item, setValue] = React.useState(() => {
-    const value = storage.getItem(key) || initialValue;
+    const value = initialValue || storage.getItem(key);
     storeItem(value);
     return value;
   });
 
-  const setItem = (value: any) => {
-    setValue(value);
-    storeItem(value);
-  };
-
   function storeItem(value: any) {
     storage.setItem(key, JSON.stringify(value));
+  }
+
+  function setItem(value: any) {
+    setValue(value);
+    storeItem(value);
   }
 
   return [item, setItem];
