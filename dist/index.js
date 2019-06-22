0038191256086e8402490f2a13abc0f8d617c4a2
Sung M. Kim <sungmkim80@gmail.com>
2019-06-22 08:47:47 -0400

    changed main to index.js after upgrading to 0.7.2

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-use-localstorage.cjs.production.js');
+  module.exports = require('./react-use-localstorage.cjs.production.min.js');
 } else {
   module.exports = require('./react-use-localstorage.cjs.development.js');
 }
