b5398bae5ee7d52df97f99c8aa362824c4e8a9f5
Sung M. Kim <sungmkim80@gmail.com>
2019-06-26 21:23:56 -0400

    Upgrade tsdx from 0.6.1 to 0.7.2 (#20)
    * changed main to index.js after upgrading to 0.7.2
    
    * 3.1.2-alpha.4
    
    * fixed 'module' file name from '*.em.*' to '*.esm.*'
    
    * 3.1.2-alpha.5
    
    * added scripts for alpha version & publish
    
    * 3.1.2-alpha.6
    
    * fixed 'module' name
    
    * 3.1.2-alpha.7
    
    * Removed 'dist' folder added wrongly
    
    * Changed default 'main' module to be unminified ESM module
    
    * 3.1.2-alpha.8
    
    * Removed 'window.' prefix for localStorage

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-use-localstorage.cjs.production.js');
-} else {
-  module.exports = require('./react-use-localstorage.cjs.development.js');
-}
