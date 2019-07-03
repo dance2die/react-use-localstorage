a0d24f8e54b12f8d09bdd6e3f9baa7d716ab7353
Sean McPherson <seanmcp25@gmail.com>
2019-07-03 14:12:31 -0400

    Test localStorage in hook

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,7 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useLocalStorage from '../src/index';
 
 describe('useLocalStorage', (): void => {
+  const KEY = 'key';
   const VALUE = {
     INITIAL: 'initial value',
     CHANGED: 'changed value',
@@ -10,32 +11,38 @@ describe('useLocalStorage', (): void => {
 
   describe('Setup', () => {
     it('Returns initial value', () => {
-      const { result } = renderHook(() =>
-        useLocalStorage('key', VALUE.INITIAL)
-      );
+      const { result } = renderHook(() => useLocalStorage(KEY, VALUE.INITIAL));
       expect(result.current[0]).toMatch(VALUE.INITIAL);
     });
 
     it('When no initial value is passed, returns an empty string', () => {
-      const { result } = renderHook(() => useLocalStorage('key'));
+      const { result } = renderHook(() => useLocalStorage(KEY));
       expect(result.current[0]).toMatch(VALUE.NONE);
     });
 
     it('Returns setValue function', () => {
-      const { result } = renderHook(() =>
-        useLocalStorage('key', VALUE.INITIAL)
-      );
+      const { result } = renderHook(() => useLocalStorage(KEY, VALUE.INITIAL));
       expect(typeof result.current[1]).toMatch('function');
     });
   });
 
-  it('When `setValue()` is called, the returned `value` updates', () => {
-    const { result } = renderHook(() => useLocalStorage('key', VALUE.INITIAL));
+  it('When `setValue()` is called, the `value` updates', () => {
+    const { result } = renderHook(() => useLocalStorage(KEY, VALUE.INITIAL));
 
     act(() => {
       result.current[1](VALUE.CHANGED);
     });
 
     expect(result.current[0]).toMatch(VALUE.CHANGED);
+  });
+
+  it('When `value` changes, `localStorage` is updated', () => {
+    const { result } = renderHook(() => useLocalStorage(KEY, VALUE.INITIAL));
+
+    act(() => {
+      result.current[1](VALUE.CHANGED);
+    });
+
+    expect(localStorage.getItem(KEY)).toBe(VALUE.CHANGED);
   });
 });
